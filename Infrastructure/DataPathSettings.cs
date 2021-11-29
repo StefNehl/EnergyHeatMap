d99e409b2b6a2527120725eab0e5bcc94246df4b
Nehl-hub <68614152+Nehl-hub@users.noreply.github.com>
2021-11-29 16:15:49 +0100

    Added json read

---
+++ Infrastructure/DataPathSettings.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EnergyHeatMap.Infrastructure
+{
+    public class DataPathSettings
+    {
+        public string? DataPathCrypto { get; set; }
+        public string? DataPathEnergy { get; set; }
+    }
+}
--- Infrastructure/Services/CryptoCoinStateService.cs
+++ Infrastructure/Services/CryptoCoinStateService.cs
@@ -3,23 +3,40 @@ using EnergyHeatMap.Contracts.Entities;
 using EnergyHeatMap.Contracts.Models;
 using EnergyHeatMap.Domain.Entities;
 using EnergyHeatMap.Domain.Extensions;
+using Microsoft.Extensions.Options;
 using System.Data;
+using System.Text.Json;
 
 namespace EnergyHeatMap.Infrastructure.Services
 {
     public class CryptoCoinStateService : ICryptoCoinStateService
     {
         private List<ICryptoCoinStateEntity> _cryptoCoinStateEntities;
-        private const string CryptoDataPath = @"C:\projects\EnergyHeatMap\Data\crypto\";
+        private readonly string CryptoDataPath;
         private const string BtcHistoricalDataFileName = @"bitcoin_2010-10-1_2021-11-29.csv";
+        private const string BtcHashRate = @"btc_hashrate.json";
 
-        public CryptoCoinStateService()
+        public CryptoCoinStateService(IOptionsMonitor<DataPathSettings> optionsMonitor)
         {
+            try
+            {
+                var path = optionsMonitor.CurrentValue.DataPathCrypto;
+                if(path == null)
+                    throw new ArgumentNullException(nameof(CryptoDataPath));
+
+                CryptoDataPath = path;
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+
             _cryptoCoinStateEntities = new List<ICryptoCoinStateEntity>();
-            LoadBtcData();
+            LoadBtcHistoricalData();
+            LoadBtcHashrateData();
         }
 
-        private bool LoadBtcData()
+        private bool LoadBtcHistoricalData()
         {
             var path = CryptoDataPath + BtcHistoricalDataFileName;
             try
@@ -53,6 +70,34 @@ namespace EnergyHeatMap.Infrastructure.Services
             return true;
         }
 
+        private bool LoadBtcHashrateData()
+        {
+            try
+            {
+                var jsonString = File.ReadAllText(CryptoDataPath + BtcHashRate);
+                var hashRateData = JsonSerializer.Deserialize<IList<HashRateDataSet>>(jsonString);
+
+                if (hashRateData == null)
+                    throw new ArgumentNullException(nameof(CryptoDataPath));
+
+                foreach (var hashRate in hashRateData)
+                {
+                    var dateTime = new DateTime(hashRate.Ticks);
+                    var hdSet = _cryptoCoinStateEntities
+                        .FirstOrDefault(i => i.DateTime == dateTime);
+
+                    if (hdSet != null)
+                        hdSet.Hashrate = hashRate.Hashrate;
+                }
+            }
+            catch 
+            {
+                throw;
+            }
+
+            return true;
+        }
+
         public async Task<IEnumerable<ICryptoCoinState>> GetAllAsync(CancellationToken ct)
         {
             await Task.Delay(5, ct);
@@ -60,6 +105,12 @@ namespace EnergyHeatMap.Infrastructure.Services
             if (_cryptoCoinStateEntities.Count == 0)
                 return new List<ICryptoCoinState>();
             return _cryptoCoinStateEntities.Select(i => i.ToModel());
+        }
+
+        private class HashRateDataSet
+        {
+            public long Ticks { get; set; }
+            public decimal Hashrate { get; set; }
         }
     }
 }
