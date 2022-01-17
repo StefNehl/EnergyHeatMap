b515efdb02f46fca2ab8fb2f220e5dc729549b61
Nehl-hub <68614152+Nehl-hub@users.noreply.github.com>
2022-01-17 18:03:25 +0100

    clean

--- Api/EndpointDefinitions/CryptoCoinStateEndpointDefinition.cs
+++ Api/EndpointDefinitions/CryptoCoinStateEndpointDefinition.cs
@@ -1,9 +1,11 @@
 ﻿using EnergyHeatMap.Contracts.Enums;
+using EnergyHeatMap.Domain;
 using EnergyHeatMap.Infrastructure.Queries;
 using EnergyHeatMap.Infrastructure.Services;
 using MediatR;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using System.Linq;
 
 namespace EnergyHeatMap.Api.EndpointDefinitions
 {
@@ -13,7 +15,12 @@ namespace EnergyHeatMap.Api.EndpointDefinitions
         {
             app.MapGet("/cryptocoinstates/", GetAllAsync);
             app.MapGet("/cryoticoinstatesfiltered/", GetFilterdAsync);
+            app.MapGet("/cryoticoinstatesfilteredwithtype/", GetFilterdWithTypeAsync);
+            app.MapGet("/cryoticoinstatesfilteredvalue/", GetFilteredValueAsync);
+            app.MapGet("/cryoticoinstatesfilteredhashrate/", GetFilteredHashrateAsync);
+            app.MapGet("/cryoticoinstatesfiltereddifficulty/", GetFilteredDifficultyAsync);
             app.MapGet("/cryptocoins/", GetCryptoCoins);
+            app.MapGet("/cryptovaluetypes/", GetCryptoValueTypes);
         }
 
         public void DefineServices(IServiceCollection services)
@@ -43,11 +50,85 @@ namespace EnergyHeatMap.Api.EndpointDefinitions
         }
 
         [Authorize(Roles = $"{Role.User},{Role.Admin}")]
+        private async Task<IResult> GetFilterdWithTypeAsync([FromServices] IMediator mediator,
+            string coinnames,
+            DateTime startdate,
+            DateTime enddate,
+            CryptoValueTypes type = CryptoValueTypes.All)
+        {
+            var coins = coinnames.Split(',');
+
+            var query = new GetFilteredCryptoCoinStatesQuery(coins, startdate, enddate);
+            var result = await mediator.Send(query);
+
+            return type switch
+            {
+                CryptoValueTypes.All => Results.Ok(result),
+                CryptoValueTypes.Hashrate => Results.Ok(result.Select(x => new Tuple<DateTime, decimal>(x.DateTime, x.Hashrate))),
+                CryptoValueTypes.Difficulty => Results.Ok(result.Select(x => new Tuple<DateTime, decimal>(x.DateTime, x.Difficulty))),
+                CryptoValueTypes.Value => Results.Ok(result.Select(x => new Tuple<DateTime, decimal>(x.DateTime, x.Value))),
+                _ => Results.Ok(result),
+            };
+        }
+
+        [Authorize(Roles = $"{Role.User}, {Role.Admin}")]
+        private async Task<IResult> GetFilteredHashrateAsync([FromServices] IMediator mediator,
+            string coinnames,
+            DateTime startdate,
+            DateTime enddate)
+        {
+            var coins = coinnames.Split(',');
+
+            var query = new GetFilteredCryptoCoinStatesQuery(coins, startdate, enddate);
+            var filteredData = await mediator.Send(query);
+            var result = filteredData.Select(x => new Tuple<DateTime, decimal>(x.DateTime, x.Hashrate));
+
+            return Results.Ok(result);
+        }
+
+        [Authorize(Roles = $"{Role.User}, {Role.Admin}")]
+        private async Task<IResult> GetFilteredDifficultyAsync([FromServices] IMediator mediator,
+            string coinnames,
+            DateTime startdate,
+            DateTime enddate)
+        {
+            var coins = coinnames.Split(',');
+
+            var query = new GetFilteredCryptoCoinStatesQuery(coins, startdate, enddate);
+            var filteredData = await mediator.Send(query);
+            var result = filteredData.Select(x => new Tuple<DateTime, decimal>(x.DateTime, x.Difficulty));
+
+            return Results.Ok(result);
+        }
+
+        [Authorize(Roles = $"{Role.User}, {Role.Admin}")]
+        private async Task<IResult> GetFilteredValueAsync([FromServices] IMediator mediator,
+            string coinnames,
+            DateTime startdate,
+            DateTime enddate)
+        {
+            var coins = coinnames.Split(',');
+
+            var query = new GetFilteredCryptoCoinStatesQuery(coins, startdate, enddate);
+            var filteredData = await mediator.Send(query);
+            var result = filteredData.Select(x => new Tuple<DateTime, decimal>(x.DateTime, x.Value));
+
+            return Results.Ok(result);
+        }
+
+        [Authorize(Roles = $"{Role.User},{Role.Admin}")]
         private async Task<IResult> GetCryptoCoins([FromServices] IMediator mediator)
         {
             var query = new GetCryptoCoinsQuery();
             var result = await mediator.Send(query);
             return Results.Ok(result);
+        }
+
+        [Authorize(Roles = $"{Role.User},{Role.Admin}")]
+        private IResult GetCryptoValueTypes([FromServices] IMediator mediator)
+        {
+            var types = Enum.GetValues(typeof(CryptoValueTypes)).Cast<CryptoValueTypes>().Select(x => x.ToString());
+            return Results.Ok(types);
         }
     }
 }
---
+++ Domain/Enums/CryptoValueTypes.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EnergyHeatMap.Domain
+{
+    public enum CryptoValueTypes
+    {
+        All = 0,
+        Value = 1,
+        Hashrate = 2,
+        Difficulty = 3
+    }
+}
