9ad2c67306d1fb1788783d1c5cc9cde0c0906736
Nehl-hub <68614152+Nehl-hub@users.noreply.github.com>
2022-02-08 14:11:42 +0100

    Added Scatterd Chart

--- EnergyHeatMap.Client/ViewModels/AnalysisViewModel.cs
+++ EnergyHeatMap.Client/ViewModels/AnalysisViewModel.cs
@@ -1,8 +1,13 @@
 ﻿using EnergyHeatMap.Infrastructure.Queries.Analysis;
 using EnergyHeatMap.Infrastructure.Queries.Chart;
+using LiveChartsCore;
+using LiveChartsCore.Defaults;
+using LiveChartsCore.SkiaSharpView;
 using MediatR;
 using Microsoft.Extensions.DependencyInjection;
 using ReactiveUI;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -14,10 +19,16 @@ namespace EnergyHeatMap.Client.ViewModels
 
         private double _correlationCoefficent;
         private double[,] _chartData = new double[5, 5];
-
+        private readonly ObservableCollection<ObservablePoint> _observableValues; 
+       
         public AnalysisViewModel()
         {
             _mediator = App.IoC.Services.GetService<IMediator>();
+            _observableValues = new ObservableCollection<ObservablePoint>();
+            Series = new ObservableCollection<ISeries>
+            {
+                new ScatterSeries<ObservablePoint>{ Values = _observableValues, GeometrySize = 2}
+            };
         }
 
         public double CorrelationCoefficent
@@ -26,32 +37,24 @@ namespace EnergyHeatMap.Client.ViewModels
             set => this.RaiseAndSetIfChanged(ref _correlationCoefficent, value);
         }
 
-        public double[,] ChartData
-        {
-            get => _chartData;
-            set => this.RaiseAndSetIfChanged(ref _chartData, value);
-        }
-
         public async Task LoadHashrateValueCoefData()
         {
             var corCoeQuery = new GetCorrelationCoefficentForHashrateAndValueQuery();
             CorrelationCoefficent = await _mediator.Send(corCoeQuery);
 
-            //var coinValueQuery = new GetAllCryptoCoinStatesQuery();
-            //var coinState = await _mediator.Send(coinValueQuery);
+            var coinValueQuery = new GetAllCryptoCoinStatesQuery();
+            var coinState = await _mediator.Send(coinValueQuery);
 
-            //var values = coinState.Select(x => x.Value).ToArray();
-            //var hashRate = coinState.Select(y => y.Hashrate).ToArray();
+            var values = coinState.Select(x => x.Value).ToArray();
+            var hashRate = coinState.Select(y => y.Hashrate).ToArray();
 
-            //ChartData = new double[values.Length, hashRate.Length];
 
-            //for(int i = 0; i < values.Length; i++)
-            //{
-            //    for(int j = 0; j < hashRate.Length; j++)
-            //    {
-            //        ChartData[i, j] = values[]
-            //    }
-            //}
+            for (int i = 0; i < values.Length; i++)
+            {
+                _observableValues.Add(new(values[i], hashRate[i]));
+            }
         }
+
+        public ObservableCollection<ISeries> Series { get; set; }
     }
 }
